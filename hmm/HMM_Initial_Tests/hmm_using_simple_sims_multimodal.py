4ff78d91734e21ce23690334440701597c0dfb7f
scottmccallum76 <scott.mccallum.42@gmail.com>
2024-02-03 16:45:21 -0500

    Initial commit for HMM development.

--- hmm/HMM_Initial_Tests/hmm_using_simple_sims_multimodal.py
+++ hmm/HMM_Initial_Tests/hmm_using_simple_sims_multimodal.py
@@ -82,14 +82,30 @@ def hmm_forward(params, data, A_trans):
     # some initializations and settings
     n = len(data)
     A_new = A_trans
+    mu_n = params['mu'][params['state'] == 'neutral'].tolist()
+    mu_p = params['mu'][params['state'] == 'sweep'].tolist()
+    sd_n = params['sd'][params['state'] == 'neutral'].tolist()
+    sd_p = params['sd'][params['state'] == 'sweep'].tolist()
+    pi_n = params['pi'][params['state'] == 'neutral'].tolist()[0]
+    pi_p = params['pi'][params['state'] == 'sweep'].tolist()[0]
 
     # Probability density values for the data using distribution 1
-    bx1 = hmm_norm_pdf(x=data, mu=params.loc[0, 'mu'], sd=params.loc[0, 'sd'])
-    alpha1 = np.array(np.log(bx1[0] * params.loc[0, 'pi']))
+    bx1_temp = hmm_norm_pdf(x=data, mu=mu_n[0], sd=sd_n[0])
+    for i in range(1, len(mu_n)):
+        bx1_temp = np.append(bx1_temp, hmm_norm_pdf(x=data, mu=mu_n[i], sd=sd_n[i]), axis=1)
+    bx1 = np.max(bx1_temp, axis=1)
+    alpha1 = np.array(np.log(bx1[0] * pi_n)).reshape((1,))
+    # bx1 = hmm_norm_pdf(x=data, mu=params.loc[0, 'mu'], sd=params.loc[0, 'sd'])  # old code for 2 distributions
+    # alpha1 = np.array(np.log(bx1[0] * params.loc[0, 'pi']))  # old code for 2 distributions
 
     # Probability density values for the data using distribution 2
-    bx2 = hmm_norm_pdf(x=data, mu=params.loc[1, 'mu'], sd=params.loc[1, 'sd'])
-    alpha2 = np.array(np.log(bx2[0] * params.loc[1, 'pi']))
+    bx2_temp = hmm_norm_pdf(x=data, mu=mu_p[0], sd=sd_p[0])
+    for i in range(1, len(mu_p)):
+        bx2_temp = np.append(bx2_temp, hmm_norm_pdf(x=data, mu=mu_p[i], sd=sd_p[i]), axis=1)
+    bx2 = np.max(bx2_temp, axis=1)
+    alpha2 = np.array(np.log(bx2[0] * pi_p)).reshape((1,))
+    # bx2 = hmm_norm_pdf(x=data, mu=params.loc[1, 'mu'], sd=params.loc[1, 'sd'])  # old code for 2 distributions
+    # alpha2 = np.array(np.log(bx2[0] * params.loc[1, 'pi']))  # old code for 2 distributions
 
     # Initial m values (slightly modified from R code)
     m1_alpha = np.array(max(alpha1, alpha2))
@@ -123,12 +139,25 @@ def hmm_backward(params, data, A_trans):
     # some initializations and settings
     n = len(data)
     A_new = A_trans
+    mu_n = params['mu'][params['state'] == 'neutral'].tolist()
+    mu_p = params['mu'][params['state'] == 'sweep'].tolist()
+    sd_n = params['sd'][params['state'] == 'neutral'].tolist()
+    sd_p = params['sd'][params['state'] == 'sweep'].tolist()
+    pi_n = params['pi'][params['state'] == 'neutral'].tolist()[0]
+    pi_p = params['pi'][params['state'] == 'sweep'].tolist()[0]
 
     # Initial values for beta1 and beta2 at t=n
     # Probability density values for the data using distribution 1
-    bx1 = hmm_norm_pdf(x=data, mu=params.loc[0, 'mu'], sd=params.loc[0, 'sd'])
+    bx1_temp = hmm_norm_pdf(x=data, mu=mu_n[0], sd=sd_n[0])
+    for i in range(1, len(mu_n)):
+        bx1_temp = np.append(bx1_temp, hmm_norm_pdf(x=data, mu=mu_n[i], sd=sd_n[i]), axis=1)
+    bx1 = np.max(bx1_temp, axis=1).reshape(len(bx1_temp), 1)
+
     # Probability density values for the data using distribution 2
-    bx2 = hmm_norm_pdf(x=data, mu=params.loc[1, 'mu'], sd=params.loc[1, 'sd'])
+    bx2_temp = hmm_norm_pdf(x=data, mu=mu_p[0], sd=sd_p[0])
+    for i in range(1, len(mu_p)):
+        bx2_temp = np.append(bx2_temp, hmm_norm_pdf(x=data, mu=mu_p[i], sd=sd_p[i]), axis=1)
+    bx2 = np.max(bx2_temp, axis=1).reshape((len(bx2_temp), 1))
 
     beta1 = np.zeros(n)
     beta1[n-1] = (np.log(1))
@@ -137,20 +166,21 @@ def hmm_backward(params, data, A_trans):
 
     for t in reversed(range(0, (n-1))):  # recall that n-2 is actually the second to last position and n-1 is the last position
       # beta for i=1
-      m1_beta_j1 = (beta1[t+1] + np.log(A_new[0,0]) + np.log(bx1[t+1]))[0]  # m when j=0 and i=0
-      m1_beta_j2 = (beta2[t+1] + np.log(A_new[0,1]) + np.log(bx2[t+1]))[0]  # m when j=1 and i=0
+      m1_beta_j1 = (beta1[t+1] + np.log(A_new[0, 0]) + np.log(bx1[t+1]))[0]  # m when j=0 and i=0
+      m1_beta_j2 = (beta2[t+1] + np.log(A_new[0, 1]) + np.log(bx2[t+1]))[0]  # m when j=1 and i=0
       m1_beta = max(m1_beta_j1, m1_beta_j2)
       beta1[t] = m1_beta + np.log(np.exp(m1_beta_j1 - m1_beta) + np.exp(m1_beta_j2 - m1_beta))
 
       # beta for i=2
       m2_beta_j1 = (beta1[t+1] + np.log(A_new[1, 0]) + np.log(bx1[t+1]))[0]  # m when j=0 and i=1
-      m2_beta_j2 = (beta2[t+1] + np.log(A_new[1, 1]) + np.log(bx2[t+1]))[0] # m when j=1 and i=1
+      m2_beta_j2 = (beta2[t+1] + np.log(A_new[1, 1]) + np.log(bx2[t+1]))[0]  # m when j=1 and i=1
       m2_beta = max(m2_beta_j1, m2_beta_j2)
       beta2[t] = m2_beta + np.log(np.exp(m2_beta_j1 - m2_beta) + np.exp(m2_beta_j2 - m2_beta))
 
     # first and second parts of m value for log-likelihood backward algorithm
-    m_beta_ll1 = (beta1[0] + np.log(params.loc[0, 'pi']) + np.log(bx1[0]))[0]
-    m_beta_ll2 = (beta2[0] + np.log(params.loc[1, 'pi']) + np.log(bx2[0]))[0]
+    m_beta_ll1 = (beta1[0] + np.log(pi_n) + np.log(bx1[0]))[0]
+    m_beta_ll2 = (beta2[0] + np.log(pi_p) + np.log(bx2[0]))[0]
+
     # m value for log likelihood, backward algorithm
     m_beta_ll = max(m_beta_ll1, m_beta_ll2)
     # Backward algorithm log likelihood
@@ -184,17 +214,6 @@ def hmm_gamma(alpha, beta, n):
     z[z_draw <= gamma1] = 1
     return z, gamma1
 
-def hmm_update_mu(z):
-    # This will need to be generalized to not expect only two distributions
-    mu1 = np.sum(data[z == 1]) / np.sum(z)
-    mu2 = np.sum(data[z == 0]) / (len(z) - np.sum(z))
-    return mu1, mu2
-
-def hmm_update_sigma(z):
-    sigma1 = np.sum((data[z == 1] - mu1_new) ** 2) / np.sum(z)
-    sigma2 = np.sum((data[z == 0] - mu2_new) ** 2) / (len(z) - np.sum(z))
-    return np.sqrt(sigma1), np.sqrt(sigma2)
-
 def hmm_update_pi(z, gamma):
     pi1 = len(z[z == 1]) / len(z)
     # pi2 = 1 - gamma[0]
@@ -217,10 +236,58 @@ def hmm_update_trans(z):
     A_trans[1, 1] = 1 - A_trans[1, 0]
     return A_trans
 
+def hmm_viterbi(params, data, A_trans):
+    # some initializations and settings
+    n = len(data)
+    mu_n = params['mu'][params['state'] == 'neutral'].tolist()
+    mu_p = params['mu'][params['state'] == 'sweep'].tolist()
+    sd_n = params['sd'][params['state'] == 'neutral'].tolist()
+    sd_p = params['sd'][params['state'] == 'sweep'].tolist()
+    pi_n = params['pi'][params['state'] == 'neutral'].tolist()[0]
+    pi_p = params['pi'][params['state'] == 'sweep'].tolist()[0]
+
+    # Probability density values for the data using distribution 1
+    bx1_temp = hmm_norm_pdf(x=data, mu=mu_n[0], sd=sd_n[0])
+    for i in range(1, len(mu_n)):
+        bx1_temp = np.append(bx1_temp, hmm_norm_pdf(x=data, mu=mu_n[i], sd=sd_n[i]), axis=1)
+    bx1 = np.max(bx1_temp, axis=1)
+
+    # Probability density values for the data using distribution 2
+    bx2_temp = hmm_norm_pdf(x=data, mu=mu_p[0], sd=sd_p[0])
+    for i in range(1, len(mu_p)):
+        bx2_temp = np.append(bx2_temp, hmm_norm_pdf(x=data, mu=mu_p[i], sd=sd_p[i]), axis=1)
+    bx2 = np.max(bx2_temp, axis=1)
+
+    p_n = []  # list to hold the log probs of being in state H
+    p_p = []  # list to hold the log probs of being in state L
+    # take the log of all the Viterbi required properties
+    bx1 = np.log(bx1)
+    bx2 = np.log(bx2)
+    A_trans = np.log(A_trans)
+    pi_n = np.log(pi_n)
+    pi_p = np.log(pi_p)
+
+    for i in range(n):
+        if i == 0:
+            # The first element (0th position)
+            p_n.append(pi_n + bx1[i])
+            p_p.append(pi_p + bx2[i])
+        else:
+            # The second element
+            # H[datat[i]] is going to be equal to the probability of data[i] given state neutral (so, bx1)
+            p_n.append(bx1[i] + np.maximum(p_n[i - 1] + A_trans[0, 0], p_p[i - 1] + A_trans[1, 0]))
+            p_p.append(bx2[i] + np.maximum(p_p[i - 1] + A_trans[1, 1], p_n[i - 1] + A_trans[0, 1]))
+
+    p_zip = np.array([p_p, p_n])
+    # return the index of the max value in each column which corresponds to states 0, 1, ...
+    # note that in the event of a tie the first index is returned. However, in a real situation a tie would be extremely
+    # unlikely.
+    path = np.argmax(p_zip, axis=0)
+
+    return path
+
 # input the data
-path = 'output/HMM_data_final.txt'
-# data = hmm_get_data(path)
-data = pd.read_csv('output/simple_sims.csv')
+data = pd.read_csv('../output/simple_sims_multimodal.csv')
 pi_temp = len(data[data['label'] == 'N']) / len(data) # directly calculate the fraction of neutral events in use in pi_list
 path_actual = data['state'] # the actual sequence of 0's and 1's contained in the data ("truth")
 data = np.array(data['value']).reshape((len(data), 1))
@@ -232,54 +299,42 @@ every mean for all of the neutral and sweep events (same for the sd list). This 
 (say) a neutral signature that might be characterized by multiple modes (same for sweeps).  However, you must be sure 
 that the 'state_list' contains the correct labels for the mu and sd entries (order matters). 
 '''
-determine_params = 'no'  # if we assume the distribution params are given then 'no', else type 'yes'
-mu_list = [3.0, 8.0]  # mean of each normal distribution
-sd_list = [2.0, 2.0]  # std deviation of each normal distribution
-pi_list = [pi_temp, 1-pi_temp]  # fraction of neutral and sweep events
-state_list = ['neutral', 'sweep']  # clas labels for each mu and sd entry
+# mu and sd values pulled directly from the simple sim generator assuming the params would be known
+mu_n = [1, 4]  # mean for neutral state normal distribution
+sd_n = [1, 1]  # sd for neutral state normal distribution
+mu_p = [7, 10]  # mean for positive state normal distribution
+sd_p = [1, 1]  # sd for positive state normal distribution
+state_n = ['neutral' for i in range(len(mu_n))]
+state_p = ['sweep' for i in range(len(mu_p))]
+pi_n = [pi_temp for i in range(len(mu_n))]
+pi_p = [1-pi_temp for i in range(len(mu_p))]
+
+mu_list = mu_n + mu_p  # mean of each normal distribution
+sd_list = sd_n + sd_p  # std deviation of each normal distribution
+pi_list = pi_n + pi_p  # fraction of neutral and sweep events
+state_list = state_n + state_p  # clas labels for each mu and sd entry
+''' Parameter setup ends here...needs work'''
 a_list = [0.95, 0.05, 0.2, 0.8]  # transition matrix in a00, a01, a10, a11 format
 
 params = hmm_init_params(mu_list=mu_list, sd_list=sd_list, pi_list=pi_list, state_list=state_list)
 A_trans = hmm_init_trans(a_list=a_list)
 
-# this will get looped for some set of iterations
-num_iter = 1  # set to 1 unless determine_params ==  'yes'
+fwd, alpha = hmm_forward(params=params, data=data, A_trans=A_trans)
+bwd, beta = hmm_backward(params=params, data=data, A_trans=A_trans)
+z, gamma = hmm_gamma(alpha=alpha, beta=beta, n=len(data))
+pi1_new, pi2_new = hmm_update_pi(z, gamma)
+A_trans = hmm_update_trans(z)
+# find the most likely path using the Viterbi algorithm
+path_viterbi = hmm_viterbi(params=params, data=data, A_trans=A_trans)
 
-for i in range(num_iter):
-    if i % 10 == 0:
-        print(i)
-    fwd, alpha = hmm_forward(params=params, data=data, A_trans=A_trans)
-    bwd, beta = hmm_backward(params=params, data=data, A_trans=A_trans)
-    z, gamma = hmm_gamma(alpha=alpha, beta=beta, n=len(data))
-    mu1_new, mu2_new = hmm_update_mu(z)
-    sigma1_new, sigma2_new = hmm_update_sigma(z)
-    pi1_new, pi2_new = hmm_update_pi(z, gamma)
-    A_trans = hmm_update_trans(z)
-    if determine_params == 'yes':
-        # update the params df with the updated values above
-        params.loc[0, 'mu'] = mu1_new
-        params.loc[1, 'mu'] = mu2_new
-        params.loc[0, 'sd'] = sigma1_new
-        params.loc[1, 'sd'] = sigma2_new
-        params.loc[0, 'pi'] = pi1_new
-        params.loc[1, 'pi'] = pi2_new
-    else:
-        params = params
-
+''' PLOT THE DATA '''
 path_pred = z
 print(A_trans)
 print(params)
-
-''' PLOT THE DATA '''
-x_axis = np.linspace(np.floor(np.min(data)), np.ceil(np.max(data)), num=100)
-dist1 = stats.norm.pdf(x_axis, loc=params.loc[0, 'mu'], scale=params.loc[0, 'sd']) * params.loc[0, 'pi']
-dist2 = stats.norm.pdf(x_axis, loc=params.loc[1, 'mu'], scale=params.loc[1, 'sd']) * params.loc[1, 'pi']
+print('pi1 after update: ', round(pi1_new, 3), ' | pi2 after update: ', round(pi2_new, 3))
 
 plt.figure(figsize=(10, 10))
 plt.hist(data, density=True, bins=75, color='black', alpha=0.2, label='Data')
-plt.plot(x_axis, dist1, color='red', label='Dist1')
-plt.plot(x_axis, dist2, color='blue', label='Dist2')
-plt.plot(x_axis, dist1 + dist2, color='purple', label='Dist1 + Dist2')
 plt.legend(loc='upper left')
 plt.xlabel('values')
 plt.ylabel('density')
@@ -287,6 +342,12 @@ plt.show()
 
 ''' CONFUSION MATRIX '''
 cm = confusion_matrix(path_actual, path_pred)
+disp = ConfusionMatrixDisplay(cm, display_labels=['Sweep', 'Neutral'])
+disp.plot()
+plt.show()
+
+''' CONFUSION MATRIX '''
+cm = confusion_matrix(path_actual, path_viterbi)
 disp = ConfusionMatrixDisplay(cm, display_labels=['Sweep', 'Neutral'])
 disp.plot()
 plt.show()
--- hmm/HMM_Initial_Tests/simple_sim_generator.py
+++ hmm/HMM_Initial_Tests/simple_sim_generator.py
@@ -12,13 +12,13 @@ Using the simple_sim output, I need to:
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import random
 
-samples = 20000
-mu_n = [1, 4]  # mean for neutral state normal distribution
-sd_n = [1, 1]  # sd for neutral state normal distribution
-mu_p = [7, 10]  # mean for positive state normal distribution
-sd_p = [1, 1]  # sd for positive state normal distribution
+save_data = 'no'
+samples = 2000
+mu_n = 3  # mean for neutral state normal distribution
+sd_n = 2  # sd for neutral state normal distribution
+mu_p = 8  # mean for positive state normal distribution
+sd_p = 2  # sd for positive state normal distribution
 
 data = pd.DataFrame(columns=['state', 'label', 'value'])
 a_tran = np.array([[0.95, 0.05], [0.2, 0.8]])
@@ -27,7 +27,7 @@ a_tran = np.array([[0.95, 0.05], [0.2, 0.8]])
 state = []; label = []; coin = []
 state.append(1)
 label.append('N')
-coin.append(np.random.normal(loc=mu_n[0], scale=sd_n[0], size=1)[0])
+coin.append(np.random.normal(loc=mu_n, scale=sd_n, size=1)[0])
 
 for i in range(1, samples):
     if state[i-1] == 1:  # if the state is neutral
@@ -35,30 +35,22 @@ for i in range(1, samples):
         if draw <= a_tran[0, 0]:  # if yes, then we stay in the neutral state
             state.append(1)
             label.append('N')
-            mu_sample = random.sample(mu_n, 1)[0]  # randomly select a mu value
-            sd_sample = random.sample(sd_n, 1)[0]  # randomly select a sd value
-            coin.append(np.random.normal(loc=mu_sample, scale=sd_sample, size=1)[0])
+            coin.append(np.random.normal(loc=mu_n, scale=sd_n, size=1)[0])
         else:  # else we bump to the positive state
             state.append(0)
             label.append('P')
-            mu_sample = random.sample(mu_p, 1)[0]
-            sd_sample = random.sample(sd_p, 1)[0]
-            coin.append(np.random.normal(loc=mu_sample, scale=sd_sample, size=1)[0])
+            coin.append(np.random.normal(loc=mu_p, scale=sd_p, size=1)[0])
 
     elif state[i - 1] == 0:  # if the state is not positive, then it must be negative (condition included anyway)
         draw = np.random.uniform(low=0, high=1, size=1)[0]
         if draw <= a_tran[1, 1]:  # if yes, then we stay in the positive state
             state.append(0)
             label.append('P')
-            mu_sample = random.sample(mu_p, 1)[0]
-            sd_sample = random.sample(sd_p, 1)[0]
-            coin.append(np.random.normal(loc=mu_sample, scale=sd_sample, size=1)[0])
+            coin.append(np.random.normal(loc=mu_p, scale=sd_p, size=1)[0])
         else:  # else we bump to the negative state
             state.append(1)
             label.append('N')
-            mu_sample = random.sample(mu_n, 1)[0]  # randomly select a mu value
-            sd_sample = random.sample(sd_n, 1)[0]  # randomly select a sd value
-            coin.append(np.random.normal(loc=mu_sample, scale=sd_sample, size=1)[0])
+            coin.append(np.random.normal(loc=mu_n, scale=sd_n, size=1)[0])
 
 data['state'] = state
 data['label'] = label
@@ -72,17 +64,24 @@ print("Count of Neutral SNPs: ", len(neutral))
 print("Count of Positive SNPs: ", len(positive))
 print("Percent of Neutral SNPs: ", round(len(neutral) / len(data) * 100, 2), ' %')
 
-data.to_csv('output/simple_sims_multimodal.csv', index=False)
+if save_data == 'yes':
+    data.to_csv('output/hmm.csv', index=False)
+"""
+Next steps:
+1. For now we will assume the distributions are known as the first objective is to recreate the N,P string using HMM
+2. Therefore, import the output from this module into a separate file that performs HMM using python
+3. Then write HMM code in a fashion similar to the final (but used packages where possible)
+"""
 
 # check the data
 plt.figure()
 plt.hist(data['value'], bins=50, color='black', alpha=0.3)
 plt.hist(neutral['value'], bins=50, color='red', alpha=0.4)
 plt.hist(positive['value'], bins=50, color='blue', alpha=0.4)
-
 plt.show()
 
-# plt.figure()
-# plt.bar(x=neutral['snp'], height=neutral['state'], color='red')
-# plt.bar(x=positive['snp'], height=positive['state'], color='blue')
-# plt.show()
+if samples < 4000:
+    plt.figure()
+    plt.bar(x=neutral['snp'], height=neutral['state']-1, color='red')
+    plt.bar(x=positive['snp'], height=positive['state']+1, color='blue')
+    plt.show()
